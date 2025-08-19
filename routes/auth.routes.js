20edd0eaf35ff7fa7f5e89c6883a70f12cc20319
sakshi-redepatil <sp06696912gmail.com>
2025-08-19 22:36:28 +0530

    chnage in auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const {userLogin,sessionLogOut}=require('../controller/auth.controller')
 
 router.post('/login',userLogin);
+
 router.post('/logout',sessionLogOut);
 
 module.exports=router;
